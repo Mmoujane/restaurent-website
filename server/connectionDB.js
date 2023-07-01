4c0a899c33467194713b54e519d2719ab21e2c7d
Mmoujane <107506228+Mmoujane@users.noreply.github.com>
2023-07-02 00:24:48 +0100

    Update connectionDB.js

--- server/connectionDB.js
+++ server/connectionDB.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
-const uri = "mongodb+srv://marwane:marwane123@systemlogin.8xnaw.mongodb.net?retryWrites=true&w=majority";
+const uri = "mongodb+srv://user:password@systemlogin.8xnaw.mongodb.net?retryWrites=true&w=majority";
 module.exports.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
